342d1a7bd634e769580a47e8999d60a7bb932a91
HugoDerave <30510534+HugoDerave@users.noreply.github.com>
2018-11-16 15:02:34 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,9 +1,8 @@
 <?php
 
 if(isset($_POST['forma'])){
-    echo 'Nom: '.$_POST['nom'].'<br>'.'Prénom: '.$_POST['prenom'].'<br>'.'Sexe: '.$_POST['sexe'].'<br>'.'Couleur cheveux: '.$_POST['couleur'].'<br>';
+    echo 'Nom: '.$_POST['nom'].'<br>Prénom: '.$_POST['prenom'].'<br>Sexe: '.$_POST['sexe'].'<br>Couleur cheveux: '.$_POST['couleur'].'<br>';
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -15,18 +14,18 @@ if(isset($_POST['forma'])){
 <body>
 <h1>Gros titre du formulaire</h1>
 
-<method="POST" action="">
+<form method="POST" action="">
 
-    <input type="text" name="nom" placeholder="Nom" id="nom" /><br>
-    <input type="text" name="prenom" placeholder="Prénom" id="prenom" /><br>
-    <br>
-    <input type="radio" name="sexe" id="radio0">Masculin<br>
-    <input type="radio" name="sexe" id="radio1">Féminin<br>
-    <br>
-    <input type="text" name="couleur" id="couleur" /><br>
+<input type="text" name="nom" placeholder="Nom" id="nom" /><br>
+<input type="text" name="prenom" placeholder="Prénom" id="prenom" /><br>
 <br>
-    <input type="submit" name="submit" id="forma" value="Envoyer" /><br>
-    <input type="reset" name="reset" value="Reset" />
+<input type="radio" name="sexe" id="radio0">Masculin<br>
+<input type="radio" name="sexe" id="radio1">Féminin<br>
+<br>
+<input type="text" name="couleur" placeholder="couleur cheveux" id="couleur" /><br>
+<br>
+<input type="submit" name="forma" id="forma" value="Envoyer" /><br>
+<input type="reset" name="reset" value="Reset" />
 </form>
 
 </body>
